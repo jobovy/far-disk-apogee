c476139433fd742d16d9d15980d7ea5ff6bcfabb
Jo Bovy <jo.bovy@gmail.com>
2015-03-26 19:37:50 +0000

    apogee-anc15: tweaks

--- py/plot_dustnearplane.py
+++ py/plot_dustnearplane.py
@@ -15,7 +15,7 @@ def plot_dustnearplane(plotname,green=False):
     if green: savefile= _SAVEFILE_GREEN
     else: savefile= _SAVEFILE_MARSHALL
     # Grid
-    ls= numpy.linspace(15.,65.,_NL)
+    ls= numpy.linspace(15.,70.,_NL)
     bs= numpy.linspace(-2.,2.,_NB)
     if not os.path.exists(savefile):
         # Setup dust map
@@ -37,7 +37,8 @@ def plot_dustnearplane(plotname,green=False):
     # Now plot
     bovy_plot.bovy_print(fig_width=8.4,fig_height=4.)
     bovy_plot.bovy_dens2d(plotthis[::-1].T,origin='lower',cmap=cm.coolwarm,
-                          interpolation='nearest',colorbar=True,shrink=0.47,
+#                          interpolation='nearest',
+                          colorbar=True,shrink=0.45,
                           vmin=0.,vmax=2.,aspect=3.,
                           xrange=[ls[-1]+(ls[1]-ls[0])/2.,
                                   ls[0]-(ls[1]-ls[0])/2.],
@@ -45,7 +46,18 @@ def plot_dustnearplane(plotname,green=False):
                                   bs[-1]+(bs[1]-bs[0])/2.],
                           xlabel=r'$l\,\mathrm{(deg)}$',
                           ylabel=r'$b\,\mathrm{(deg)}$',
-                          zlabel=r'$A_H\,(\mathrm{mag})$')
+                          zlabel=r'$A_H\,(\mathrm{mag})$',
+                          zorder=0)
+    bovy_plot.bovy_text(r'$D = 7\,\mathrm{kpc}$',top_left=True,
+                        color='w',size=16.)
+    # Overplot fields
+    glons= [34.,64.]
+    glats= [0.,0.]
+    xs= numpy.linspace(-1.5,1.5,201)
+    ys= numpy.sqrt(1.5**2.-xs**2.)
+    for glon,glat in zip(glons,glats):
+        bovy_plot.bovy_plot(xs+glon,ys+glat,'w-',overplot=True,zorder=1,lw=2.)
+        bovy_plot.bovy_plot(xs+glon,-ys+glat,'w-',overplot=True,zorder=1,lw=2.)
     bovy_plot.bovy_end_print(plotname)
     return None
 
